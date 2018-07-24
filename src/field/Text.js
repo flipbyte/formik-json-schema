3b23208eaec8a228053b13d1583b07458fe5e55b
Eon Sequeira <eon@earncef.com>
2018-07-24 20:18:19 +0530

    Minor modifications.

--- src/field/Text.js
+++ src/field/Text.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 import { getIn } from 'formik';
 
 const Text = ({ config, formikProps }) => {
-    const { name, label, type, renderer, attributes, fieldType, defaultValue } = config;
+    const {
+        name,
+        label,
+        type,
+        renderer,
+        attributes,
+        fieldType,
+        defaultValue,
+        icon,
+        wrapperClass = 'form-group'
+    } = config;
     const { values, errors, setFieldValue, handleChange } = formikProps;
 
     const error = getIn(errors, name);
 
     return (
-        <div className="form-group">
+        <div className={ wrapperClass }>
             { !!label && <label>{ label }</label> }
+            { !!icon &&
+                <span className="input-group-addon">
+                    <i className={ icon }></i>
+                </span>
+            }
             <input
                 id={ name }
                 name={ name }
