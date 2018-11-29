e912ed3afef30eca5bc9368cb41180333bfb91c8
Eon Sequeira <eon@earncef.com>
2018-11-29 19:05:47 +0530

    exported Label and ErrorMessage components.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import Form from './Form';
 import './Field';
 import './Container';
+import Label from './Field/Label';
+import ErrorMessage from './Field/ErrorMessage';
 
 import * as utils from './utils';
 import * as registry from './registry';
 
-export { Form, registry, utils }
+export { Form, Label, ErrorMessage, registry, utils }
