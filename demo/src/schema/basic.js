f7043156da550af0df8bf5fb9953103786d3d99c
ES <eon@earncef.com>
2019-02-03 11:01:37 +0530

    Fixed demo mobile menu and added fields to basic form

--- demo/src/schema/basic.js
+++ demo/src/schema/basic.js
@@ -4,18 +4,44 @@ export default {
     onSubmit: save.bind(this),
     initialValues: {},
     schema: {
+        validation: {
+            name: "required|min:3",
+            email: "required|email",
+            telephone: "regex:/^\\d{3}-\\d{3}-\\d{4}$/",
+            message: "required"
+        },
         form: {
             id: "basic",
             label: "Basic",
             type: "container",
             renderer: "form",
             elements: {
-                title: {
-                    name: "title",
-                    label: "Title",
+                name: {
+                    name: "name",
+                    label: "Name",
                     type: "field",
                     renderer: "text",
                     fieldType: "text"
+                },
+                email: {
+                    name: "email",
+                    label: "Email",
+                    type: "field",
+                    renderer: "text",
+                    fieldType: "text"
+                },
+                telephone: {
+                    name: "telephone",
+                    label: "Telephone",
+                    type: "field",
+                    renderer: "text",
+                    fieldType: "telephone"
+                },
+                message: {
+                    name: "message",
+                    label: "Message",
+                    type: "field",
+                    renderer: "textarea",
                 },
                 save: {
                     type: "field",
