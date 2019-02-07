c64466b85f817ad7b67f7c67e04151cfd1125bd3
ES <eon@earncef.com>
2019-02-07 19:19:14 +0530

    Modified schema object
    - Removed "validation" key.
    - "schema" now hold the form config.
    - Validations are defined at the field level.

--- src/Form.js
+++ src/Form.js
@@ -2,27 +2,39 @@ import React from 'react';
 import { Formik } from 'formik';
 import messages from './messages';
 import Element from './Element';
-
+import { prepareValidationSchema } from './utils';
 const Validator = require('validatorjs');
 
-const validate = ( validationSchema = {}, values ) => {
-    let validation = new Validator(values, validationSchema, messages);
-    if(!validation.fails()) {
-        return {};
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.validationSchema = prepareValidationSchema(this.props.schema) || {};
+        this.validate = this.validate.bind(this);
     }
 
-    return validation.errors.all();
-}
+    validate(values) {
+        let validation = new Validator(values, this.validationSchema, messages);
+        if(!validation.fails()) {
+            return {};
+        }
 
-const Form = ({
-    schema: { validation, form },
-    initialValues = {},
-    ...props
-}) =>
-    <Formik
-        { ...props }
-        initialValues={ initialValues }
-        validate={ validate.bind(this, validation || {}) }
-        render={ props => <Element config={ form } /> } />
+        return validation.errors.all();
+    }
+
+    render() {
+        const {
+            schema,
+            initialValues = {},
+            ...rest
+        } = this.props;
+
+        return <Formik
+            { ...rest }
+            initialValues={ initialValues }
+            validate={ this.validate }
+            render={ props => <Element config={ schema } /> } />
+    }
+}
 
 export default Form;
--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,63 @@
 import _ from 'lodash';
 
-export const hasError = ( name, submitCountToValidate = 0, { errors, touched, submitCount } ) =>
-    _.get(errors, name, false) && ( _.get(touched, name, false) || submitCount > submitCountToValidate );
+export const hasError = (name, submitCountToValidate = 0, {
+    errors,
+    touched,
+    submitCount
+}) => _.get(errors, name, false) && (_.get(touched, name, false) || submitCount > submitCountToValidate);
 
-export const changeHandler = ( handler, formikProps, { onChange, ...fieldConfig }, data ) => {
+export const changeHandler = (handler, formikProps, {
+    onChange,
+    ...fieldConfig
+}, data) => {
     handler(data);
     _.isFunction(onChange) && onChange(formikProps, fieldConfig, data);
 }
 
-export const setFieldValueWrapper = ( setFieldValue, name ) => ( value ) =>
-    setFieldValue(name, value);
+export const setFieldValueWrapper = (setFieldValue, name) => (value) => setFieldValue(name, value);
+export const joinNames = (...args) => _.join(_.filter(args, arg => (_.isString(arg) && arg) || _.isInteger(arg)), '.')
 
-export const joinNames = ( ...args ) =>
-    _.join(_.filter(args, arg => (_.isString(arg) && arg) || _.isInteger(arg)), '.')
+export const prepareValidationSchema = (schema, result = {}) => {
+    const processContainer = (container) => {
+        const {
+            elements,
+            prefixNameToElement,
+            name: containerName,
+            type,
+            renderer: containerRenderer
+        } = container;
+        let prefix = prefixNameToElement || containerRenderer === 'editable-grid' ? containerName : '';
+
+        _.map(elements, (element, key, index) => {
+            const {
+                type,
+                renderer: fieldRenderer,
+                isWildcardValidation,
+                name: fieldName,
+                validation
+            } = element;
+            if (type !== 'field') {
+                let joinArgs = [fieldName];
+                if (prefix) {
+                    joinArgs.unshift(prefix, '*');
+                }
+                processContainer({
+                    ...element,
+                    name: fieldRenderer === 'editable-grid' || isWildcardValidation ? joinNames(...joinArgs) : '',
+                });
+            }
+
+            if (!validation) {
+                return;
+            }
+
+            let validationKey = containerRenderer !== 'editable-grid' ?
+                joinNames(prefix, fieldName) :
+                joinNames(prefix, '*', fieldName);
+            result[validationKey] = validation;
+        })
+    }
+
+    processContainer(schema);
+    return result;
+}
