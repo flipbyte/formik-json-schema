050b496346b6b4cb263b7e6ca5fad541513ef8c9
ES <eon@earncef.com>
2019-03-29 10:10:37 +0530

    Fixed custom container renderer issue (#20)
    * Exported Element component for creating custom containers.
    
    * 0.2.2
    
    * Exported Element component

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import './Container';
 import Label from './Field/Label';
 import ErrorMessage from './Field/ErrorMessage';
 import FieldTemplate from './FieldTemplate';
+import Element from './Element';
 
 import * as utils from './utils';
 import * as registry from './registry';
 
-export { Form, Label, ErrorMessage, FieldTemplate, registry, utils }
+export { Form, Label, Element, ErrorMessage, FieldTemplate, registry, utils }
