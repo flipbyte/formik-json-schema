8da55a1c48ca1f97df2cafb7dba405a9858df26b
ES <eon@earncef.com>
2019-04-16 11:44:52 +0530

    Added onUpdate callback to Form

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,22 @@
 import _ from 'lodash';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Formik } from 'formik';
 import messages from './messages';
 import Element from './Element';
 import { SchemaProvider } from './withFormConfig';
 import { prepareValidationSchema } from './utils';
 import Rules from '@flipbyte/yup-schema';
+
+const FormikForm = ({ onUpdate, schema, ...formik }) => {
+    // on form value(s) change
+    useEffect(() => {
+        if(typeof onUpdate === 'function') {
+            onUpdate(formik);
+        }
+    }, [formik.values]);
+
+    return <Element config={ schema } />;
+};
 
 class Form extends React.Component {
     constructor(props) {
@@ -31,12 +42,13 @@ class Form extends React.Component {
     render() {
         const {
             schema,
+            onUpdate = () => {},
             initialValues = {},
             ...rest
         } = this.props;
 
         const formProps = { ...rest, initialValues };
-        if(null !== this.validationSchema) {
+        if (null !== this.validationSchema) {
             formProps.validationSchema = this.validationSchema;
         }
 
@@ -44,10 +56,12 @@ class Form extends React.Component {
             <SchemaProvider value={ this.getContextValue() }>
                 <Formik
                     { ...formProps }
-                    render={ props => <Element config={ schema } /> }
+                    render={(props) => (
+                        <FormikForm onUpdate={ onUpdate } schema={ schema } { ...props } />
+                    )}
                 />
             </SchemaProvider>
-        )
+        );
     }
 }
 
