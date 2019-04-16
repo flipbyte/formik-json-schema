5987f66ac580283357dac9b48c051878d36c0d87
ES <eon@earncef.com>
2019-04-16 11:44:00 +0530

    Update FieldTemplate.js

--- src/FieldTemplate.js
+++ src/FieldTemplate.js
@@ -2,14 +2,12 @@ import React from 'react';
 import Label from './Field/Label';
 import ErrorMessage from './Field/ErrorMessage';
 
-const FieldTemplate = ({ name, label, labelClass, formGroupClass, children }) => {
-    return (
-        <div className={ formGroupClass }>
-            <Label htmlFor={ name } className={ labelClass }>{ label }</Label>
-            { children }
-            <ErrorMessage name={ name } />
-        </div>
-    );
-}
+const FieldTemplate = ({ name, label, labelClass, formGroupClass, children }) => (
+    <div className={ formGroupClass }>
+        <Label htmlFor={ name } className={ labelClass }>{ label }</Label>
+        { children }
+        <ErrorMessage name={ name } />
+    </div>
+);
 
 export default FieldTemplate;
