efa4ebfe085261774f33510b583390656cd60a5b
ES <eon@earncef.com>
2019-08-09 09:09:27 +0530

    Minor changes

--- src/withFormConfig.js
+++ src/withFormConfig.js
@@ -3,14 +3,16 @@ import React, { Component } from 'react';
 import { prepareValidationSchema } from './utils';
 
 const SchemaContext = React.createContext({});
-export const SchemaProvider = ({ value, children }) =>
+export const SchemaProvider = ({ value, children }) => (
     <SchemaContext.Provider value={ value }>
         { children }
     </SchemaContext.Provider>
+);
 
-const withFormConfig = WrappedComponent => props =>
+const withFormConfig = WrappedComponent => props => (
     <SchemaContext.Consumer>
         { config => <WrappedComponent { ...props } { ...config } /> }
     </SchemaContext.Consumer>
+);
 
 export default withFormConfig
