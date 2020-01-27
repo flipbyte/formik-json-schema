c5a5999a786e4d8ce74b809b3308803d5471c3eb
Andrew Ringler <aringler@payette.com>
2020-01-27 12:53:52 -0500

    validate from validate prop instead of creating validation schema

--- src/Form.js
+++ src/Form.js
@@ -20,34 +20,36 @@ const FormikForm = ({ onUpdate, schema, ...formik }) => {
     return <Element config={ schema } />;
 };
 
-const Form = React.forwardRef(({ schema, onUpdate = () => {}, initialValues = {}, ...rest }, ref) => {
+const Form = React.forwardRef(({ schema, onUpdate = () => {}, initialValues = {}, prepareValidationSchemaFromValidationProps = true, ...rest }, ref) => {
     const [ validationSchema, setValidationSchema ] = useState(null);
+    const formProps = { ...rest, initialValues };
 
     /**
      * Initialize validation schema.
      *
      * Convert the validation schema rules from yup-schema array to yup object
      */
-    const initValidationSchema = useCallback(() => {
-        const yupSchema = prepareValidationSchema(schema);
-        const validationSchema = !_.isEmpty(yupSchema) ? new Rules([[ 'object', yupSchema ]]).toYup() : null;
-        setValidationSchema(validationSchema);
-    }, [ schema ]);
+    if(prepareValidationSchemaFromValidationProps) {
+      const initValidationSchema = useCallback(() => {
+          const yupSchema = prepareValidationSchema(schema);
+          const validationSchema = !_.isEmpty(yupSchema) ? new Rules([[ 'object', yupSchema ]]).toYup() : null;
+          setValidationSchema(validationSchema);
+      }, [ schema ]);
 
-    /**
-     * Everytime the schema changes re-initialize validationSchema
-     *
-     * This is has to be done everytime schema changes because,
-     * certain cases may involve dynamically changing form fields based on
-     * certain conditions, routes etc.
-     */
-    useEffect(() => {
-        initValidationSchema();
-    }, [ schema ]);
+      /**
+       * Everytime the schema changes re-initialize validationSchema
+       *
+       * This is has to be done everytime schema changes because,
+       * certain cases may involve dynamically changing form fields based on
+       * certain conditions, routes etc.
+       */
+      useEffect(() => {
+          initValidationSchema();
+      }, [ schema ]);
 
-    const formProps = { ...rest, initialValues };
-    if (null !== validationSchema) {
-        formProps.validationSchema = validationSchema;
+      if (null !== validationSchema) {
+          formProps.validationSchema = validationSchema;
+      }
     }
 
     return (
