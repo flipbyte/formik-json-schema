64f02b4825189c5519198845de6bf136563a926b
ES <eon@earncef.com>
2020-03-16 12:12:17 +0530

    Comment for form container demo

--- demo/src/schema/containers.js
+++ demo/src/schema/containers.js
@@ -24,6 +24,7 @@ export default {
         label: "Containers",
         type: "container",
         renderer: "form",
+        comment: "Form level comment. Contrary to popular belief, Lorem Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of ",
         elements: {
             save: {
                 type: "field",
