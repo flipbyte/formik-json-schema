e44ca1d44b7d7a6af04ad795ffaea15f02348102
ES <eon@earncef.com>
2020-03-16 11:01:40 +0530

    Allow comments for fields

--- demo/src/schema/all-available-fields.js
+++ demo/src/schema/all-available-fields.js
@@ -25,7 +25,8 @@ export default {
                 label: "Text",
                 type: "field",
                 renderer: "text",
-                fieldType: "text"
+                fieldType: "text",
+                comment: "This is a field comment. You can add your text here."
             },
             autocomplete: {
                 name: "autocomplete",
@@ -48,6 +49,7 @@ export default {
                 name: "react-select",
                 label: "React Select",
                 isCreatable: true,
+                comment: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
                 options: [{
                     value: 0,
                     label: "No"
