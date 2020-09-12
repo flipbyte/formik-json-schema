e3ec77a768e993723dbdb8d3bb6c292cbe018b80
Ishaan Puniani <ishaan_puniani@yahoo.com>
2020-09-12 15:36:02 +0200

    Added inner-html field

--- demo/src/schema/all-available-fields.js
+++ demo/src/schema/all-available-fields.js
@@ -84,6 +84,15 @@ export default {
                 htmlClass: 'text-muted d-block mb-3 mt-1',
                 defaultValue: 'This is some raw html text content: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum'
             },
+            innerHtml: {
+                type: "field",
+                renderer: "inner-html",
+                name: "innerText",
+                as: "p",
+                htmlClass: "text-muted d-block mb-3 mt-1",
+                defaultValue:
+                    "<h2>This is some raw html content</h2>Lorem Ipsum is simply dummy text of the <b>printing and typesetting industry</b>. Lorem Ipsum has been the industry's standard dummy text ever <u>since the 1500s</u>, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+            },
             autocomplete: {
                 name: "autocomplete",
                 label: "Autocomplete",
